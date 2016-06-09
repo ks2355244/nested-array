4c8ad852fdb8ed2e0534d5854c282e8d804c62bf
ks2355244 <ks2355244@yahoo.com>
2016-06-09 14:52:32 +0800

    2nd commit

--- nested_arrays.rb
+++ nested_arrays.rb
@@ -1,9 +1,10 @@
 # Objective 2: Chessboard
-table = [[12, "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
-		[9, "Ms. Buckets", "Point Guard", [19, 0, 11, 22, 0]],
-		[31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
-		[18, "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]],
-		[22, "MK DiBoux", "Small Forward", [11, 0, 23, 17, 0]]
+table = [["Number", "Name", "Position", "Points per Game"],
+         [12,"Joe Schmo","Center",[14, 32, 7, 0, 23] ],
+         [9, "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0] ],
+         [31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25] ],
+         [7, "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19] ],
+         [22, "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0] ]]
 
 # Objective 3: Data table
 def table(m,n)
